be6cdf16541c4b368464002f5e2d68d635079744
Aniketh Reddy <sunnyrockzzz1811@gmail.com>
2025-11-03 12:05:30 +0530

    Implement Audio Player

--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,49 @@
 import { FlatList, Text, View } from "react-native";
 import Meme from "../components/meme.jsx"
 
+
 const data = [
   {
     name: "bald dance",
-    thumbnail: "../assets/data/bald dance/thumbnail.jpeg",
-    sound: "../assets/data/bald dance/sound.mp3"
+    thumbnail: require("../assets/data/bald dance/thumbnail.jpeg"),
+    sound: require("../assets/data/bald dance/sound.mp3")
   },
   {
     name: "bing chilling",
-    thumbnail: "../assets/data/bing chilling/thumbnail.jpeg",
-    sound: "../assets/data/bing chilling/sound.mp3"
+    thumbnail: require("../assets/data/bing chilling/thumbnail.jpeg"),
+    sound: require("../assets/data/bing chilling/sound.mp3")
   },
   {
     name: "ching cheng",
-    thumbnail: "../assets/data/ching cheng/thumbnail.jpeg",
-    sound: "../assets/data/ching cheng/sound.mp3"
+    thumbnail: require("../assets/data/ching cheng/thumbnail.jpeg"),
+    sound: require("../assets/data/ching cheng/sound.mp3")
   },
   {
     name: "eggman",
-    thumbnail: "../assets/data/eggman/thumbnail.jpeg",
-    sound: "../assets/data/eggman/sound.mp3"
+    thumbnail: require("../assets/data/eggman/thumbnail.jpeg"),
+    sound: require("../assets/data/eggman/sound.mp3")
   },
   {
     name: "monkey singing",
-    thumbnail: "../assets/data/monkey singing/monkey_singing.png",
-    sound: "../assets/data/monkey singing/Chinese monkeys singing (English Translation).mp3"
+    thumbnail: require("../assets/data/monkey singing/monkey_singing.png"),
+    sound: require("../assets/data/monkey singing/Chinese monkeys singing (English Translation).mp3")
   },
   {
     name: "oohooh",
-    thumbnail: "../assets/data/oohooh/thumbnail.jpeg",
-    sound: "../assets/data/oohooh/audio.mp3"
+    thumbnail: require("../assets/data/oohooh/thumbnail.jpeg"),
+    sound: require("../assets/data/oohooh/audio.mp3")
   },
   {
     name: "rising sun",
-    thumbnail: "../assets/data/rising sun/thumbnail.jpeg",
-    sound: "../assets/data/rising sun/sound.mp3"
+    thumbnail: require("../assets/data/rising sun/thumbnail.jpeg"),
+    sound: require("../assets/data/rising sun/sound.mp3")
   },
   {
     name: "superidol",
-    thumbnail: "../assets/data/superidol/thumbnail.jpeg",
-    sound: "../assets/data/superidol/sound.mp3"
+    thumbnail: require("../assets/data/superidol/thumbnail.jpeg"),
+    sound: require("../assets/data/superidol/sound.mp3")
   }
 ];
-
 export default function Index() {
   return (
     <View
@@ -53,7 +53,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <FlatList data={data} renderItem={({ item }) => <Meme name={item.name} />} />
+      <FlatList data={data} renderItem={({ item }) => <Meme name={item.name} sound={item.sound} thumbnail={item.thumbnail} />} />
     </View>
   );
 }
--- components/meme.jsx
+++ components/meme.jsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native"
-
+import { View, Text, Button, Image } from "react-native"
+import { useAudioPlayer } from "expo-audio"
 function Meme(props) {
+	const player = useAudioPlayer(props.sound);
 	return (
 		<View>
-			<Text> Hello World! {props.name} </Text>
+			<Text> World! {props.name} </Text>
+			<Button title="Play" onPress={() => player.play()} />
+			<Image source={props.thumbnail} />
 		</ View>)
 }
 
