84bc4e1a82024916da0f47e16169897c15bf4975
Aniketh Reddy <sunnyrockzzz1811@gmail.com>
2025-11-03 12:12:31 +0530

    Improve UI

--- components/meme.jsx
+++ components/meme.jsx
@@ -1,13 +1,11 @@
-import { View, Text, Button, Image } from "react-native"
+import { View, Text, Button, Image, TouchableHighlight } from "react-native"
 import { useAudioPlayer } from "expo-audio"
 function Meme(props) {
 	const player = useAudioPlayer(props.sound);
 	return (
-		<View>
-			<Text> World! {props.name} </Text>
-			<Button title="Play" onPress={() => player.play()} />
+		<TouchableHighlight onPress={() => player.play()}>
 			<Image source={props.thumbnail} />
-		</ View>)
+		</ TouchableHighlight>)
 }
 
 export default Meme
